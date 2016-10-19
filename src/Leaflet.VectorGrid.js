c02f8062ada02eada49024f3a59a7863a0b0bd73
Per Liedman <per.liedman@dotnetmentor.se>
2016-10-19 13:27:25 +0200

    Add support for displaying points as circle markers

--- src/Leaflet.VectorGrid.js
+++ src/Leaflet.VectorGrid.js
@@ -27,51 +27,42 @@ L.VectorGrid = L.GridLayer.extend({
 				for (var i in layer.features) {
 					var feat = layer.features[i];
 
-					if (feat.type > 1) { // Lines, polygons
+					/// Style can be a callback that is passed the feature's
+					/// properties and tile zoom level...
+					var styleOptions = (layerStyle instanceof Function) ?
+					layerStyle(feat.properties, coords.z) :
+					layerStyle;
 
-						this._mkFeatureParts(feat, pxPerExtent);
+					this._mkFeatureParts(feat, pxPerExtent);
 
-						/// Style can be a callback that is passed the feature's
-						/// properties and tile zoom level...
-						var styleOptions = (layerStyle instanceof Function) ?
-						layerStyle(feat.properties, coords.z) :
-						layerStyle;
+					if (!(styleOptions instanceof Array)) {
+						styleOptions = [styleOptions];
+					}
 
-						if (!(styleOptions instanceof Array)) {
-							styleOptions = [styleOptions];
+					/// Style can be an array of styles, for styling a feature
+					/// more than once...
+					for (var j in styleOptions) {
+						var style = L.extend({}, L.Path.prototype.options, styleOptions[j]);
+
+						if (feat.type === 2) {	// Polyline
+							style.fill = false;
 						}
 
-						/// Style can be an array of styles, for styling a feature
-						/// more than once...
-						for (var j in styleOptions) {
-							var style = L.extend({}, L.Path.prototype.options, styleOptions[j]);
+						feat.options = style;
+						renderer._initPath( feat );
+						renderer._updateStyle( feat );
 
-							if (feat.type === 1) { // Points
-								style.fill = false;
-							} else if (feat.type === 2) {	// Polyline
-								style.fill = false;
-							}
-
-							feat.options = style;
-							renderer._initPath( feat );
-							renderer._updateStyle( feat );
-
-							if (feat.type === 1) { // Points
-								// 							style.fill = false;
-							} else if (feat.type === 2) {	// Polyline
-								style.fill = false;
-								renderer._updatePoly(feat, false);
-							} else if (feat.type === 3) {	// Polygon
-								renderer._updatePoly(feat, true);
-							}
-
-							renderer._addPath( feat );
+						if (feat.type === 1) { // Points
+							feat._radius = style.radius,
+							renderer._updateCircle( feat );
+						} else if (feat.type === 2) {	// Polyline
+							style.fill = false;
+							renderer._updatePoly(feat, false);
+						} else if (feat.type === 3) {	// Polygon
+							renderer._updatePoly(feat, true);
 						}
 
-					} else {
-						// Feat is a point (type === 1)
-
-						/// FIXME!!!
+						renderer._addPath( feat );
 					}
 				}
 
@@ -88,26 +79,36 @@ L.VectorGrid = L.GridLayer.extend({
 	// pretty much as L.Polyline._projectLatLngs and L.Polyline._clipPoints
 	// would do but simplified as the vectors are already simplified/clipped.
 	_mkFeatureParts: function(feat, pxPerExtent) {
+		var coord;
 
-		var rings = feat.geometry;
-
-		feat._parts = [];
-		for (var i in rings) {
-			var ring = rings[i];
-			var part = [];
-			for (var j in ring) {
-				var coord = ring[j];
-				if ('x' in coord) {
-					// Protobuf vector tiles return {x: , y:}
-					part.push(L.point(coord.x * pxPerExtent, coord.y * pxPerExtent));
-				} else {
-					// Geojson-vt returns [,]
-					part.push(L.point(coord[0] * pxPerExtent, coord[1] * pxPerExtent));
-				}
+		if (feat.type === 1) {
+			// Point
+			coord = feat.geometry[0][0];
+			if ('x' in coord) {
+				feat._point = L.point(coord.x * pxPerExtent, coord.y * pxPerExtent);
+				feat._empty = L.Util.falseFn;
 			}
-			feat._parts.push(part);
-		}
+		} else {
+			// Polylines and polygons
+			var rings = feat.geometry;
 
+			feat._parts = [];
+			for (var i in rings) {
+				var ring = rings[i];
+				var part = [];
+				for (var j in ring) {
+					coord = ring[j];
+					if ('x' in coord) {
+						// Protobuf vector tiles return {x: , y:}
+						part.push(L.point(coord.x * pxPerExtent, coord.y * pxPerExtent));
+					} else {
+						// Geojson-vt returns [,]
+						part.push(L.point(coord[0] * pxPerExtent, coord[1] * pxPerExtent));
+					}
+				}
+				feat._parts.push(part);
+			}
+		}
 	},
 
 });
