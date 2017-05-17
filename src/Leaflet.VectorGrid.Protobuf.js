043e62a4d5c27139311a0073aa129df247f65737
Tero Laitinen <tero.laitinen@iki.fi>
2017-05-17 11:19:29 +0300

    option fetchOptions added to VectorGrid.Protobuf to send additional headers/cookies with requests

--- src/Leaflet.VectorGrid.Protobuf.js
+++ src/Leaflet.VectorGrid.Protobuf.js
@@ -58,7 +58,11 @@ L.VectorGrid.Protobuf = L.VectorGrid.extend({
 		//
 		// 🍂option subdomains: String = 'abc'
 		// Akin to the `subdomains` option for `L.TileLayer`.
-		subdomains: 'abc',	// Like L.TileLayer
+    subdomains: 'abc',	// Like L.TileLayer
+    //
+    // 🍂option fetchOptions: Object = {}
+    // options passed to `fetch`, e.g. {credentials: 'same-origin'} to send cookie for the current domain
+    fetchOptions: {}
 	},
 
 	initialize: function(url, options) {
@@ -88,9 +92,7 @@ L.VectorGrid.Protobuf = L.VectorGrid.extend({
 
 		var tileUrl = L.Util.template(this._url, L.extend(data, this.options));
 
-    return fetch(tileUrl, {
-      credentials: 'same-origin'
-    }).then(function(response){
+    return fetch(tileUrl, this.options.fetchOptions).then(function(response){
 
 			if (!response.ok) {
 				return {layers:[]};
